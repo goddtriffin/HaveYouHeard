5fb929c1f5921863e857988502646f9ba7393397
Todd Griffin <nemagnusfrater@gmail.com>
2017-10-16 01:21:17 -0400

    Add functionality to Yes button to open McDonald's app if installed. Add local fields to temporarily track lies, heards, and notHeards.

--- app/src/main/java/net/magnusfrater/haveyouheard/MainActivity.java
+++ app/src/main/java/net/magnusfrater/haveyouheard/MainActivity.java
@@ -16,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     private Button bYes;
     private Button bNo;
 
+    // temp
+    private int heardCount = 0;
+    private int notHeardCount = 0;
+    private int liedCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,13 +59,55 @@ public class MainActivity extends AppCompatActivity {
 
     // heard about the McDonald's's app
     private void yes () {
-
+        if (Utils.isAppInstalled(this, "com.mcdonalds.app")) {
+            // has McDonald's app installed
+            heard();
+            launchMcDonaldsApp();
+        } else {
+            // lied, doesn't have McDonald's app installed
+            lied();
+            launchPlayStore();
+        }
     }
 
     // didn't hear about the McDonald's's app
     private void no () {
+        if (Utils.isAppInstalled(this, "com.mcdonalds.app")) {
+            // lied, has McDonald's app installed
+            lied();
+            launchMcDonaldsApp();
+        } else {
+            // doesn't have McDonald's app installed
+            notHeard();
+            launchPlayStore();
+        }
+    }
+
+    // opens McDonald's app link in the play store
+    private void launchPlayStore () {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("market://details?id=com.mcdonalds.app"));
         startActivity(intent);
+    }
+
+    private void launchMcDonaldsApp () {
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.mcdonalds.app");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    // if user lied about hearing about the McDonald's app
+    private void lied () {
+        liedCount++;
+    }
+
+    // if user actually heard about the McDonald's app
+    private void heard () {
+        heardCount++;
+    }
+
+    // if user actually hasn't heard about the McDonald's app
+    private void notHeard () {
+        notHeardCount++;
     }
 }
