514d6a85c467374c101dc04df718f3850a4dcee2
Todd Griffin <nemagnusfrater@gmail.com>
2017-11-21 03:13:12 -0500

    Add functionality that allows realtime increasing each 'count'.

--- app/src/main/java/net/magnusfrater/haveyouheard/MainActivity.java
+++ app/src/main/java/net/magnusfrater/haveyouheard/MainActivity.java
@@ -37,8 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private final DocumentReference dr = db.collection("HaveYouHeard").document("4vvi2PovaZMRd2ZYJhz6");
+    private final DocumentReference dr = FirebaseFirestore.getInstance().collection("HaveYouHeard").document("4vvi2PovaZMRd2ZYJhz6");
 
     // temp
     private int heardCount = 0;
@@ -58,6 +57,27 @@ public class MainActivity extends AppCompatActivity {
 
         // initialize Firebase
         initFirebase();
+    }
+
+    @Override
+    protected void onStart () {
+        super.onStart();
+
+        // listens for 'count' changes
+        dr.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+                if (documentSnapshot.exists()) {
+                    int heardCount      = (int) Math.round(documentSnapshot.getDouble("heardCount"));
+                    int liedCount       = (int) Math.round(documentSnapshot.getDouble("liedCount"));
+                    int notHeardCount   = (int) Math.round(documentSnapshot.getDouble("notHeardCount"));
+
+                    updateHeardCount(heardCount);
+                    updateLiedCount(liedCount);
+                    updateNotHeardCount(notHeardCount);
+                }
+            }
+        });
     }
 
     // set up the views
@@ -103,22 +123,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-
-        // listens for 'count' changes
-        dr.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
-                if (documentSnapshot.exists()) {
-                    int heardCount      = (int) Math.round(documentSnapshot.getDouble("heardCount"));
-                    int liedCount       = (int) Math.round(documentSnapshot.getDouble("liedCount"));
-                    int notHeardCount   = (int) Math.round(documentSnapshot.getDouble("notHeardCount"));
-
-                    updateHeardCount(heardCount);
-                    updateLiedCount(liedCount);
-                    updateNotHeardCount(notHeardCount);
-                }
-            }
-        });
     }
 
     // signs the user in through Firebase anonymous auth
@@ -181,15 +185,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void heard () {
-        // TODO
+        updateCount("heardCount");
     }
 
     private void lied () {
-        // TODO
+        updateCount("liedCount");
     }
 
     private void notHeard () {
-        // TODO
+        updateCount("notHeardCount");
+    }
+
+    // handles `count++` on specified count
+    private void updateCount (final String countName) {
+        dr.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        // only handle update if snapshot still exists, and the specified 'count' exists
+                        if (documentSnapshot.exists() && documentSnapshot.contains(countName)) {
+                            // get latest count
+                            int count = (int) Math.round(documentSnapshot.getDouble(countName));
+
+                            // count++
+                            dr.update(countName, count + 1);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Failed to load " + countName + "...", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
     // opens McDonald's app link in the play store
